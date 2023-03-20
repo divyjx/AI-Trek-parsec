4a8d882d9b7dc0bcfb5b4c8f6d9a7ae5229d0075
divyjx <divyjx@gmail.com>
2023-03-20 13:09:52 +0530

    first commit to D

--- players/player_blue.py
+++ players/player_blue.py
@@ -42,7 +42,7 @@ def tick(state: State) -> List[Action]:
         flag = 0 # flag to check if we have given an action
         agent = state.agents[agent_id]
 
-        # alive conditions added
+        # alive conditions added branch D
         if not agent.is_alive():
             action = Action(agent_id,UPDATE_DIRECTION,Point(agent.get_direction().x,
                                   agent.get_direction().y))
